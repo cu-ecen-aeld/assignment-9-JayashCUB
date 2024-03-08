6825d91c49b162f318ea56f647090f0b9490b475
jayasharun.raulkar@colorado.edu <jayasharun.raulkar@colorado.edu>
2024-03-07 22:41:38 -0700

    Assg 7 part 2 full test success

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '3742b4a1c38e99662464f926ffc5d92842908d47'
+AESD_ASSIGNMENTS_VERSION = '6525465a1ac217801b9777e709aaf89cc5e62144'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:

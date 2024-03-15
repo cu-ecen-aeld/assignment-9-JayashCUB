083c82af44f8fccc92056b1306bc686111f2d684
jayasharun.raulkar@colorado.edu <jayasharun.raulkar@colorado.edu>
2024-03-15 01:13:23 -0600

    Assg 8 changes

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '6525465a1ac217801b9777e709aaf89cc5e62144'
+AESD_ASSIGNMENTS_VERSION = '5ea697f2bc6b16ce73eebe5e99cff6cf81fbe3e3'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
